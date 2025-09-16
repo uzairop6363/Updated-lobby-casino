9e9a150e3658755e92c655a0a183b64b97a98255
uzairop6363 <uzairop6363@gmail.com>
2025-09-16 23:54:26 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -24,7 +24,33 @@ function App() {
           <div className="game-logo">⚡</div>
           <div className="game-name">Game 3</div>
         </div>
+        <div className="game-item">
+          <div className="game-logo">🎲</div>
+          <div className="game-name">Game 4</div>
+        </div>
       </div>
 
       {/* Bottom Bar */}
-      <div className="bottom-bar"
+      <div className="bottom-bar">
+        <div className="bottom-option">
+          <div className="icon">🏠</div>
+          Home
+        </div>
+        <div className="bottom-option">
+          <div className="icon">🎮</div>
+          Games
+        </div>
+        <div className="bottom-option">
+          <div className="icon">💰</div>
+          Coins
+        </div>
+        <div className="bottom-option">
+          <div className="icon">⚙️</div>
+          Settings
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
